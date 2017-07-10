e027ab72e135d4601868ff5e14dfe2bf3931ec2c
joshgressman <joshgressman@gmail.com>
2017-07-10 13:49:39 -0500

    added animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 //Animations are just the jorney from one state to the next
 
@@ -13,7 +13,9 @@ import { trigger, state, style } from '@angular/animations';
       })),
       state('highlighted', style({
         backgroundColor: 'blue', transform: 'translateX(100px)'
-      }))
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(600))
     ])
   ]
 })
@@ -32,4 +34,5 @@ export class AppComponent {
   onAnimate(){
     this.state == 'normal' ? this.state = 'highlighted' : this.state='normal';
   }
+
 }
