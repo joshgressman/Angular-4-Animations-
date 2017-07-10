5ef63660f125b92977382fdbf53f19bec5a8f5d7
joshgressman <joshgressman@gmail.com>
2017-07-10 14:24:14 -0500

    added shrink animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,29 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('highlighted', style({
         backgroundColor: 'blue', transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(600))
+    ]),
+    trigger('wildState',[
+      state('normal', style({
+        'background-color': 'red', transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue', transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green', transform: 'translateX(0px) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(600))
+      transition('highlighted => normal', animate(600)),
+      transition('shrunken <=> *', animate(500)),
     ])
   ]
 })
 export class AppComponent {
 	list = ['Milk', 'Sugar', 'Bread'];
   state = 'normal';
+  wildState = 'normal';
 
 	onAdd(item) {
 		this.list.push(item);
@@ -33,6 +48,13 @@ export class AppComponent {
 
   onAnimate(){
     this.state == 'normal' ? this.state = 'highlighted' : this.state='normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState='normal';
   }
+
+  onShrink(){
+    this.wildState ='shrunken';
+  }
+
+
 
 }
