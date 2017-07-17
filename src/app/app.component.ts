7e0c6091103df340c7a0e416b9c3d4c390755b1d
joshgressman <joshgressman@gmail.com>
2017-07-17 18:56:48 -0500

    added animataion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 //Animations are just the jorney from one state to the next
 
@@ -51,7 +51,49 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         }),
         animate(300)
     ]),
+    transition('* => void',[
+      animate(400, style({
+        transform: 'translateX(100px)',
+        opacity: 0,
+      })),
   ]),
+  ]),
+  trigger('list2',[
+    state('in', style({
+      opacity: 1,
+      transform: 'translateX(0)'
+    })),
+    transition('void => *', [
+      animate(1000, keyframes([
+        style({
+          transform: 'translateX(-100px)',
+          opacity: 0,
+          offset: 0
+        }),
+        style({
+          transform: 'translateX(-50px)',
+          opacity: 0.5,
+          offset: 0.3
+        }),
+        style({
+          transform: 'translateX(-20px)',
+          opacity: 1,
+          offset: 0.8
+        }),
+        style({
+          transform: 'translateX(0px)',
+          opacity: 1,
+          offset: 1
+        })
+      ]))
+    ]),
+  transition('* => void',[
+    animate(400, style({
+      transform: 'translateX(100px)',
+      opacity: 0,
+    })),
+]),
+])
   ]
 })
 export class AppComponent {
